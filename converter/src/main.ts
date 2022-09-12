0b805fb93495ff2955ef3c705fbf01e73d2f0f91
KorvinAtreides <Korvin1312b@yandex.by>
2022-09-12 12:39:44 +0300

    feat: remove spinner when loaded

--- converter/src/main.ts
+++ converter/src/main.ts
@@ -1,13 +1,24 @@
-import currencyRequest from "./api/currency-request";
+import currencyRequest from './api/currency-request';
 
 export default class Main {
-  async startApp() {
+
+  currencyData: string;
+
+  createSpinner() {
     const spinner = document.createElement('img');
     spinner.src = './img/spinner.png';
     spinner.classList.add('spinIcon');
+    return spinner;
+  }
+
+  async startApp() {
+    const spinner = this.createSpinner();
     document.body.append(spinner);
-    
-    const my = await currencyRequest();
-    console.log(my.data)
+    const currencyPromise = currencyRequest();
+    currencyPromise.then((response) => {
+      this.currencyData = response.data;
+      console.log(this.currencyData);
+      spinner.remove();
+    })
   }
 }
