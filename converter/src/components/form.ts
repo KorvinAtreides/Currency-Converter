f224197f13fd5a55a580bb9b7ad00e8bf8a48227
KorvinAtreides <Korvin1312b@yandex.by>
2022-09-12 16:59:38 +0300

    refactor: create Form class

--- converter/src/components/form.ts
+++ converter/src/components/form.ts
@@ -8,53 +8,65 @@ const FORM_CURRENCY_OPTIONS_STYLE = 'form--currency-options';
 const CURRENCIES_LIST = ['RUS', 'BYN', 'USD', 'EUR'];
 const MAX_DIGITS = 8;
 
-function createSelect() {
-  const select = document.createElement('select');
-  select.classList.add(FORM_CURRENCY_OPTIONS_STYLE);
-  CURRENCIES_LIST.forEach((currency) => {
-    const option = document.createElement('option');
-    option.textContent = currency;
-    select.append(option);
-  });
-  return select;
-}
-
 function validateInput(input: HTMLInputElement) {
   if(Number(input.value) >= Number(input.max)) input.value = input.max;
   if(Number(input.value) <= 0) input.value = '0';
   input.value = String(Number(input.value));
 }
 
+export default class Form {
+  HTMLForm: HTMLFormElement;
 
-export default function createForm() {
-  const form = document.createElement('form');
-  form.classList.add(FORM_STYLE);
-  const title = document.createElement('h3');
-  title.classList.add(FORM_TITLE_STYLE);
-  title.textContent = FORM_TITLE_TEXT;
+  sourceInput: HTMLInputElement;
 
-  const sourceWrapper = document.createElement('div');
-  sourceWrapper.classList.add(FORM_CONTENT_WRAPPER_STYLE);
-  const sourceInput = document.createElement('input');
-  sourceInput.classList.add(FORM_INPUT_STYLE);
-  sourceInput.placeholder = FORM_INPUT_PLACEHOLDER_TEXT;
-  sourceInput.type = 'number';
-  sourceInput.min = '0';
-  sourceInput.max = String(Math.floor(10 ** MAX_DIGITS - 1));
-  sourceInput.addEventListener('input', () => {
-    validateInput(sourceInput);
-  });
-  const sourceCurrency = createSelect();
-  sourceWrapper.append(sourceInput, sourceCurrency);
+  selectSourceCurrency: HTMLSelectElement;
 
-  const targetWrapper = document.createElement('div');
-  targetWrapper.classList.add(FORM_CONTENT_WRAPPER_STYLE);
-  const targetInput = document.createElement('input');
-  targetInput.classList.add(FORM_INPUT_STYLE);
-  targetInput.readOnly = true;
-  const targetCurrency = createSelect();
-  targetWrapper.append(targetInput, targetCurrency);
+  selectTargetCurrency: HTMLSelectElement;
 
-  form.append(title, sourceWrapper, targetWrapper);
-  return form;
+  constructor() {
+    this.HTMLForm = document.createElement('form');
+    this.HTMLForm.classList.add(FORM_STYLE);
+  }
+
+  createFormComponents() {
+    const title = document.createElement('h3');
+    title.classList.add(FORM_TITLE_STYLE);
+    title.textContent = FORM_TITLE_TEXT;
+
+    const sourceWrapper = document.createElement('div');
+    sourceWrapper.classList.add(FORM_CONTENT_WRAPPER_STYLE);
+    this.sourceInput = document.createElement('input');
+    this.sourceInput.classList.add(FORM_INPUT_STYLE);
+    this.sourceInput.placeholder = FORM_INPUT_PLACEHOLDER_TEXT;
+    this.sourceInput.type = 'number';
+    this.sourceInput.min = '0';
+    this.sourceInput.max = String(Math.floor(10 ** MAX_DIGITS - 1));
+    this.sourceInput.addEventListener('input', () => {
+      validateInput(this.sourceInput);
+    });
+    this.selectSourceCurrency = Form.createSelect();
+    sourceWrapper.append(this.sourceInput, this.selectSourceCurrency);
+
+    const targetWrapper = document.createElement('div');
+    targetWrapper.classList.add(FORM_CONTENT_WRAPPER_STYLE);
+    const targetInput = document.createElement('input');
+    targetInput.classList.add(FORM_INPUT_STYLE);
+    targetInput.readOnly = true;
+    targetInput.style.cursor = 'not-allowed';
+    this.selectTargetCurrency = Form.createSelect();
+    targetWrapper.append(targetInput, this.selectTargetCurrency);
+
+    this.HTMLForm.append(title, sourceWrapper, targetWrapper);
+  }
+
+  static createSelect() {
+    const select = document.createElement('select');
+    select.classList.add(FORM_CURRENCY_OPTIONS_STYLE);
+    CURRENCIES_LIST.forEach((currency) => {
+      const option = document.createElement('option');
+      option.textContent = currency;
+      select.append(option);
+    });
+    return select;
+  }
 }
--- converter/src/main.ts
+++ converter/src/main.ts
@@ -1,6 +1,6 @@
 import currencyRequest from './api/currency-request';
 import convertFromXmlToArray from './api/handle-response';
-import createForm from './components/form';
+import Form from './components/form';
 import createSpinner from './components/spinner';
 import { ConvertRecord } from './interfaces';
 
@@ -8,15 +8,18 @@ export default class Main {
 
   currencyData: Array<ConvertRecord>;
 
+  form: Form;
+
   async startApp() {
     const spinner = createSpinner();
     document.body.append(spinner);
-    const form = createForm();
+    this.form = new Form();
+    this.form.createFormComponents();
     const currencyPromise = currencyRequest();
     currencyPromise.then((response) => {
       spinner.remove();
       this.currencyData = convertFromXmlToArray(response.data);
-      document.body.append(form);
+      document.body.append(this.form.HTMLForm);
       console.log(this.currencyData);
     })
   }
